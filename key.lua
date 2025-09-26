702c077cef4c1237ab250599c725d400bd82b95d
csxshop <csxshop888@gmail.com>
2025-09-26 23:41:44 +0700

    key.lua

--- key.lua
+++ key.lua
@@ -2,9 +2,10 @@ return {
     ["HWID"] = "shiro",
     ["86ac48af96f14192"] = "haward",
     ["770E69817BFB4383"] = "shiro",
-    ["9a9f5997f6a147e2"] = "snotong",
     ["DF79E33154AC4696"] = "nalin",
+    ["9a9f5997f6a147e2"] = "snotong",
     ["98617d948cd5450e"] = "snotong",
     ["edf6346942a147aa"] = "snotong",
     ["478724eca01647ed"] = "snotong",
+    ["d2101b3f83e14443"] = "snotong",
 }
